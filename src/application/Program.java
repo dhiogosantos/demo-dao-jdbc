af99b14c44e697cab14c86dc2866ecfc5d0406ec
Dhiogo Santos <dhiogo.santos70@gmail.com>
2025-03-26 00:48:38 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,20 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Books");
+        Seller obj1 = new Seller(4, "Bob", "bob@gmail.com", new Date(), 3000.4, obj);
 
         System.out.println(obj);
 
-    }
+        System.out.println(obj1);
 
+    }
 }
